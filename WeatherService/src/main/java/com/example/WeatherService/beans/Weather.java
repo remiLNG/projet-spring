1f97ff67fc144964119a7d7a75f37fdf7c4f3909
Remi <remi.longin@outlook.fr>
2022-02-18 15:47:01 +0100

    User story 3 - normalement je possede le swag

--- WeatherService/src/main/java/com/example/WeatherService/beans/Weather.java
+++ WeatherService/src/main/java/com/example/WeatherService/beans/Weather.java
@@ -1,9 +1,15 @@
 package com.example.WeatherService.beans;
 
+import io.swagger.annotations.ApiModelProperty;
+
 public class Weather {
+    @ApiModelProperty(notes = "City Name", name = "ville", required = true, value = "Marseille")
     private String ville;
+    @ApiModelProperty(notes = "City's zipcode", name = "zipCode", required = true, value = "13000")
     public String zipCode;
+    @ApiModelProperty(notes = "City's country", name = "pays", required = true, value = "France")
     public String pays;
+    @ApiModelProperty(notes = "City's weather", name = "weather", required = true, value = "SUPER BON CE SOLEIL")
     public String weather;
 
     public Weather(String ville, String zipCode, String pays, String weather){
--- WeatherService/src/main/java/com/example/WeatherService/controller/WeatherController.java
+++ WeatherService/src/main/java/com/example/WeatherService/controller/WeatherController.java
@@ -2,6 +2,10 @@ package com.example.WeatherService.controller;
 
 
 import com.example.WeatherService.beans.Weather;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,6 +16,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
+@Api(value = "WeatherService", description = "Service to retrieve weather")
 public class WeatherController {
     public static final List<Weather> weathers = new ArrayList<Weather>(){
         private static final long serialVersionUID = -3970206781360313502L;
@@ -22,6 +27,13 @@ public class WeatherController {
         }
     };
 
+
+    @ApiOperation(value = "Get weather by city", response = Weather.class, tags = "getWeatherByCity")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Suceess|OK"),
+            @ApiResponse(code = 401, message = "not authorized!"),
+            @ApiResponse(code = 403, message = "forbidden!!!"),
+            @ApiResponse(code = 404, message = "city not found") })
     @RequestMapping(name="getWeatherByCity", method= RequestMethod.GET,value="/ville/{ville}")
     public List<Weather> getWeatherByCity(@PathVariable String ville){
         try {
@@ -34,6 +46,12 @@ public class WeatherController {
         }
     }
 
+    @ApiOperation(value = "Get weathers by zipcode", response = Weather.class, tags = "getWeatherByZipCode")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Suceess|OK"),
+            @ApiResponse(code = 401, message = "not authorized!"),
+            @ApiResponse(code = 403, message = "forbidden!!!"),
+            @ApiResponse(code = 404, message = "zipcode not found") })
     @RequestMapping(name="getWeatherByZipCode", method= RequestMethod.GET,value="/zipcode/{zipCode}")
     public List<Weather> getWeatherByZipCode(@PathVariable String zipCode){
         try {
@@ -46,6 +64,13 @@ public class WeatherController {
         }
     }
 
+
+    @ApiOperation(value = "Get weathers by country", response = Weather.class, responseContainer = "List", tags = "getWeatherByPays")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Suceess|OK"),
+            @ApiResponse(code = 401, message = "not authorized!"),
+            @ApiResponse(code = 403, message = "forbidden!!!"),
+            @ApiResponse(code = 404, message = "country not found") })
     @RequestMapping(name="getWeatherByPays", method= RequestMethod.GET,value="/pays/{pays}")
     public List<Weather> getWeatherByPays(@PathVariable String pays){
         try {
